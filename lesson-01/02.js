ae3d7de7b208d9b929b3faf07a138d21ba2ad38a
unknown <krutko7779@gmail.com>
2023-12-07 20:35:04 +0300

    lession-01 task 03

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -6,5 +6,5 @@ if (number1 % 2 === 0) {
 	console.log(number1, ':Это четное число');
 } else {
 	console.log(number1, ':Это нечетное число');
-}
+};
 
---
+++ lesson-01/03.js
@@ -0,0 +1,15 @@
+// cod
+
+let number = 1;
+
+let result1 = number++;
+console.log(result1); 
+
+let result2 = ++number;
+console.log(result2); 
+
+let result3 = number--;
+console.log(result3); 
+
+let result4 = --number;
+console.log(result4); 
